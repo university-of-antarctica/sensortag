71606cb41203fda3bdab322e63fd354c483bb8e8
gpwclark <gpwclark@gmail.com>
2015-05-04 17:53:11 -0400

    pausing to work on webserver now, working on getting noble communicating with ti sensortag

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sensortag
 Central device made with noble to interact with TI sensortag peripheral
+
+configuration instructions for nginx
+http://stackoverflow.com/questions/5009324/node-js-nginx-what-now
--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 var noble = require('noble');
 
-noble.startScanning();
+noble.on('stateChange', function(state) {
+  if (state === 'poweredOn') {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+
+noble.on('discover', function(peripheral) {
+  if (peripheral.address === "bc:6a:29:ac:53:f5"){
+    console.log(peripheral.address);
+  }
+
+});
